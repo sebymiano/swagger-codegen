13fdddda2269f84285ee09a9219d8284d856d12d
sebymiano <mianosebastiano@gmail.com>
2017-06-05 23:57:35 +0200

    Implemented Pistache Server Codegen

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PistacheServerCodegen.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/languages/PistacheServerCodegen.java
@@ -1,14 +1,14 @@
 package io.swagger.codegen.languages;
 
 
-import io.swagger.codegen.CodegenConfig;
-import io.swagger.codegen.CodegenType;
-import io.swagger.codegen.DefaultCodegen;
-import io.swagger.codegen.SupportingFile;
+import io.swagger.codegen.*;
+import io.swagger.models.Model;
+import io.swagger.models.Operation;
+import io.swagger.models.Response;
+import io.swagger.models.Swagger;
+import io.swagger.models.properties.*;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.*;
 
 public class PistacheServerCodegen extends DefaultCodegen implements CodegenConfig {
     @Override
@@ -36,6 +36,8 @@ public class PistacheServerCodegen extends DefaultCodegen implements CodegenConf
 
         apiTemplateFiles.put("api-header.mustache", ".h");
         apiTemplateFiles.put("api-source.mustache", ".cpp");
+        apiTemplateFiles.put("api-impl-header.mustache", ".h");
+        apiTemplateFiles.put("api-impl-source.mustache", ".cpp");
 
         embeddedTemplateDir = templateDir = "pistache-server";
 
@@ -45,8 +47,7 @@ public class PistacheServerCodegen extends DefaultCodegen implements CodegenConf
 
         supportingFiles.add(new SupportingFile("modelbase-header.mustache", "model", "ModelBase.h"));
         supportingFiles.add(new SupportingFile("modelbase-source.mustache", "model", "ModelBase.cpp"));
-        supportingFiles.add(new SupportingFile("restserver-header.mustache", "", "RestServer.h"));
-        supportingFiles.add(new SupportingFile("restserver-source.mustache", "", "RestServer.cpp"));
+        supportingFiles.add(new SupportingFile("server.mustache", "", "server.cpp"));
 
 
         languageSpecificPrimitives = new HashSet<String>(
@@ -71,11 +72,175 @@ public class PistacheServerCodegen extends DefaultCodegen implements CodegenConf
         importMapping.put("std::vector", "#include <vector>");
         importMapping.put("std::map", "#include <map>");
         importMapping.put("std::string", "#include <string>");
-        importMapping.put("HttpContent", "#include \"HttpContent.h\"");
         importMapping.put("Object", "#include \"Object.h\"");
         importMapping.put("utility::string_t", "#include <cpprest/details/basic_types.h>");
         importMapping.put("utility::datetime", "#include <cpprest/details/basic_types.h>");
 
+    }
+
+    @Override
+    public void processOpts() {
+        super.processOpts();
+
+        additionalProperties.put("modelNamespaceDeclarations", modelPackage.split("\\."));
+        additionalProperties.put("modelNamespace", modelPackage.replaceAll("\\.", "::"));
+        additionalProperties.put("apiNamespaceDeclarations", apiPackage.split("\\."));
+        additionalProperties.put("apiNamespace", apiPackage.replaceAll("\\.", "::"));
+    }
+
+    /**
+     * Escapes a reserved word as defined in the `reservedWords` array. Handle
+     * escaping those terms here. This logic is only called if a variable
+     * matches the reseved words
+     *
+     * @return the escaped term
+     */
+    @Override
+    public String escapeReservedWord(String name) {
+        return "_" + name; // add an underscore to the name
+    }
+
+    @Override
+    public String toModelImport(String name) {
+        if (importMapping.containsKey(name)) {
+            return importMapping.get(name);
+        } else {
+            return "#include \"" + name + ".h\"";
+        }
+    }
+
+    @Override
+    public CodegenModel fromModel(String name, Model model, Map<String, Model> allDefinitions) {
+        CodegenModel codegenModel = super.fromModel(name, model, allDefinitions);
+
+        Set<String> oldImports = codegenModel.imports;
+        codegenModel.imports = new HashSet<>();
+        for (String imp : oldImports) {
+            String newImp = toModelImport(imp);
+            if (!newImp.isEmpty()) {
+                codegenModel.imports.add(newImp);
+            }
+        }
+
+        return codegenModel;
+    }
+
+    @Override
+    public CodegenOperation fromOperation(String path, String httpMethod, Operation operation,
+                                          Map<String, Model> definitions, Swagger swagger) {
+        CodegenOperation op = super.fromOperation(path, httpMethod, operation, definitions, swagger);
+
+        if (operation.getResponses() != null && !operation.getResponses().isEmpty()) {
+            Response methodResponse = findMethodResponse(operation.getResponses());
+
+            if (methodResponse != null) {
+                if (methodResponse.getSchema() != null) {
+                    CodegenProperty cm = fromProperty("response", methodResponse.getSchema());
+                    op.vendorExtensions.put("x-codegen-response", cm);
+                    if(cm.datatype == "HttpContent")
+                    {
+                        op.vendorExtensions.put("x-codegen-response-ishttpcontent", true);
+                    }
+                }
+            }
+        }
+
+        String pathForPistache = path.replaceAll("\\{(.*?)}$", ":$1");
+        op.vendorExtensions.put("x-codegen-pistache-path", pathForPistache);
+
+        return op;
+    }
+
+    @Override
+    public String toModelFilename(String name) {
+        return initialCaps(name);
+    }
+
+    @Override
+    public String toApiFilename(String name) {
+        return initialCaps(name) + "Api";
+    }
+
+    /**
+     * Optional - type declaration. This is a String which is used by the
+     * templates to instantiate your types. There is typically special handling
+     * for different property types
+     *
+     * @return a string value used as the `dataType` field for model templates,
+     *         `returnType` for api templates
+     */
+    @Override
+    public String getTypeDeclaration(Property p) {
+        String swaggerType = getSwaggerType(p);
+
+        if (p instanceof ArrayProperty) {
+            ArrayProperty ap = (ArrayProperty) p;
+            Property inner = ap.getItems();
+            return getSwaggerType(p) + "<" + getTypeDeclaration(inner) + ">";
+        }
+        if (p instanceof MapProperty) {
+            MapProperty mp = (MapProperty) p;
+            Property inner = mp.getAdditionalProperties();
+            return getSwaggerType(p) + "<utility::string_t, " + getTypeDeclaration(inner) + ">";
+        }
+        if (p instanceof StringProperty || p instanceof DateProperty
+                || p instanceof DateTimeProperty || p instanceof FileProperty
+                || languageSpecificPrimitives.contains(swaggerType)) {
+            return toModelName(swaggerType);
+        }
+
+        return "std::shared_ptr<" + swaggerType + ">";
+    }
+
+    @Override
+    public String toDefaultValue(Property p) {
+        if (p instanceof StringProperty) {
+            return "U(\"\")";
+        } else if (p instanceof BooleanProperty) {
+            return "false";
+        } else if (p instanceof DateProperty) {
+            return "utility::datetime()";
+        } else if (p instanceof DateTimeProperty) {
+            return "utility::datetime()";
+        } else if (p instanceof DoubleProperty) {
+            return "0.0";
+        } else if (p instanceof FloatProperty) {
+            return "0.0f";
+        } else if (p instanceof IntegerProperty || p instanceof BaseIntegerProperty) {
+            return "0";
+        } else if (p instanceof LongProperty) {
+            return "0L";
+        } else if (p instanceof DecimalProperty) {
+            return "0.0";
+        } else if (p instanceof MapProperty) {
+            MapProperty ap = (MapProperty) p;
+            String inner = getSwaggerType(ap.getAdditionalProperties());
+            return "std::map<utility::string_t, " + inner + ">()";
+        } else if (p instanceof ArrayProperty) {
+            ArrayProperty ap = (ArrayProperty) p;
+            String inner = getSwaggerType(ap.getItems());
+            if (!languageSpecificPrimitives.contains(inner)) {
+                inner = "std::shared_ptr<" + inner + ">";
+            }
+            return "std::vector<" + inner + ">()";
+        } else if (p instanceof RefProperty) {
+            RefProperty rp = (RefProperty) p;
+            return "new " + toModelName(rp.getSimpleRef()) + "()";
+        }
+        return "nullptr";
+    }
+
+    @Override
+    public void postProcessParameter(CodegenParameter parameter) {
+        super.postProcessParameter(parameter);
+
+        boolean isPrimitiveType = parameter.isPrimitiveType == Boolean.TRUE;
+        boolean isListContainer = parameter.isListContainer == Boolean.TRUE;
+        boolean isString = parameter.isString == Boolean.TRUE;
+
+        if (!isPrimitiveType && !isListContainer && !isString && !parameter.dataType.startsWith("std::shared_ptr")) {
+            parameter.dataType = "std::shared_ptr<" + parameter.dataType + ">";
+        }
     }
 
     /**
@@ -92,6 +257,69 @@ public class PistacheServerCodegen extends DefaultCodegen implements CodegenConf
      */
     @Override
     public String apiFileFolder() {
-        return outputFolder;
+        return outputFolder + "/api";
+    }
+
+    /**
+     * Optional - swagger type conversion. This is used to map swagger types in
+     * a `Property` into either language specific types via `typeMapping` or
+     * into complex models if there is not a mapping.
+     *
+     * @return a string value of the type or complex model for this property
+     * @see io.swagger.models.properties.Property
+     */
+    @Override
+    public String getSwaggerType(Property p) {
+        String swaggerType = super.getSwaggerType(p);
+        String type = null;
+        if (typeMapping.containsKey(swaggerType)) {
+            type = typeMapping.get(swaggerType);
+            if (languageSpecificPrimitives.contains(type))
+                return toModelName(type);
+        } else
+            type = swaggerType;
+        return toModelName(type);
+    }
+
+    @Override
+    public String toModelName(String type) {
+        if (typeMapping.keySet().contains(type) || typeMapping.values().contains(type)
+                || importMapping.values().contains(type) || defaultIncludes.contains(type)
+                || languageSpecificPrimitives.contains(type)) {
+            return type;
+        } else {
+            return Character.toUpperCase(type.charAt(0)) + type.substring(1);
+        }
+    }
+
+    @Override
+    public String toVarName(String name) {
+        if (typeMapping.keySet().contains(name) || typeMapping.values().contains(name)
+                || importMapping.values().contains(name) || defaultIncludes.contains(name)
+                || languageSpecificPrimitives.contains(name)) {
+            return name;
+        }
+
+        if (name.length() > 1) {
+            return Character.toUpperCase(name.charAt(0)) + name.substring(1);
+        }
+
+        return name;
+    }
+
+    @Override
+    public String toApiName(String type) {
+        return Character.toUpperCase(type.charAt(0)) + type.substring(1) + "Api";
+    }
+
+    @Override
+    public String escapeQuotationMark(String input) {
+        // remove " to avoid code injection
+        return input.replace("\"", "");
+    }
+
+    @Override
+    public String escapeUnsafeCharacters(String input) {
+        return input.replace("*/", "*_/").replace("/*", "/_*");
     }
 }
